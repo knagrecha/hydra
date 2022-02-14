8d32264836c2008b2ab460a79bdd8e22cbd6b5c7
knagrecha <32966638+knagrecha@users.noreply.github.com>
2022-02-13 17:05:30 -0800

    Update twelve-model-task-vit.py

--- examples/twelve-model-task-vit.py
+++ examples/twelve-model-task-vit.py
@@ -305,7 +305,7 @@ def main():
      errors in partitioning memory consumption.
     """
     
-    orchestra.buffer = 20000
+    orchestra.buffer = 25000
 
     orchestra.generate()
     orchestra.train_models()
