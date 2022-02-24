db964c3caf022af29608c1bfaffbe4eccd1ff6fe
knagrecha <32966638+knagrecha@users.noreply.github.com>
2022-02-24 14:58:13 -0800

    Update Backward.py

--- hydra/components/executor/Backward.py
+++ hydra/components/executor/Backward.py
@@ -25,39 +25,42 @@ class Backward():
         self.type="Backward"
         self.idx = idx
 
-    def run(self, model, optimizer, batch_input, device, back_input, scaler=None):
+    def run(self, model, optimizer, batch_input, device, back_input, entry_point, scaler=None):
         
         model.to(device, non_blocking=True)
         model.zero_grad()  # zeroes the gradient buffers of all parameters
         optimizer.zero_grad()  # zero the gradient buffers
 
+        # Pass Back Input
         if not isinstance(back_input, torch.Tensor):
             #print("Back input is a list")
-            toy_input = [x.to(device, non_blocking=True) for x in back_input]
+            back_input = [x.to(device, non_blocking=True) for x in back_input]
 
             if self.idx != 0:
-                for m_input in toy_input:
+                for m_input in back_input:
                     #print(m_input)
                     if isinstance(m_input, torch.Tensor):
                         m_input.requires_grad_(True)
 
         else:
-            toy_input = back_input.to(device, non_blocking=True)
+            back_input = back_input.to(device, non_blocking=True)
             if self.idx != 0:
-                toy_input.requires_grad_(True)     
+                back_input.requires_grad_(True)     
 
+        # Gradients
         if not isinstance(batch_input, torch.Tensor):
             batch_input = [x.to(device, non_blocking=True) for x in batch_input]
         else:
-            batch_input = batch_input.to(device, non_blocking=True) 
+            batch_input = batch_input.to(device, non_blocking=True)
+            
+        # Backprop entry point
+        if not isinstance(entry_point, torch.Tensor):
+            entry_point = [x.to(device, non_blocking=True) for x in entry_point]
+        else:
+            entry_point = entry_point.to(device, non_blocking=True) 
 
 
-        with torch.cuda.amp.autocast():
-            toy_output = model(toy_input)
-        
-        if scaler is not None:
-            toy_output = scaler.scale(toy_output)
-        torch.autograd.backward(toy_output, batch_input)
+        torch.autograd.backward(entry_point, batch_input)
         del toy_output
         del batch_input
         pass_back_gradients = None
