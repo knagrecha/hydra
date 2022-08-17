4e40d1ceb0594ffb5e9daa4deb99588a8f067394
knagrecha <32966638+knagrecha@users.noreply.github.com>
2022-08-17 16:22:22 -0700

    Cleanup

--- hydra/components/executor/Backward.py
+++ hydra/components/executor/Backward.py
@@ -11,7 +11,7 @@
 # limitations under the License.
 # ==============================================================================
 
-from hydra.utilities import delete_batch, move_batch_to_device
+from hydra.utilities import delete_batch, move_batch_to_device, track_gradients, untrack_gradients
 import torch
 
 
@@ -27,44 +27,32 @@ class Backward():
         self.idx = idx
 
     def run(self, model, optimizer, batch_input, device, back_input, scaler=None):
-        
         model.to(device, non_blocking=True)
-        if not isinstance(back_input, torch.Tensor):
-            toy_input = [x.to(device, non_blocking=True) for x in back_input]
-            if self.idx != 0:
-                for m_input in toy_input:
-                    if isinstance(m_input, torch.Tensor):
-                        m_input.requires_grad_(True)
-
-        else:
-            toy_input = back_input.to(device, non_blocking=True)
-            if self.idx != 0:
-                toy_input.requires_grad_(True)     
-
-        if not isinstance(batch_input, torch.Tensor):
-            batch_input = [x.to(device, non_blocking=True) for x in batch_input]
-        else:
-            batch_input = batch_input.to(device, non_blocking=True) 
-
+        toy_input = move_batch_to_device(back_input, device)
+        
+        if self.idx != 0:
+            track_gradients(toy_input)
+            
+        batch_input = move_batch_to_device(batch_input)
 
         with torch.cuda.amp.autocast():
             toy_output = model(toy_input)
         
         if scaler is not None:
             toy_output = scaler.scale(toy_output)
+            
         torch.autograd.backward(toy_output, batch_input)
-        del toy_output
-        del batch_input
+        
+        del toy_output, batch_input
+        
         pass_back_gradients = None
+        
+        
+        
         if self.idx != 0: # the first backwards pass need not compute back pass gradients.
-            if (not isinstance(toy_input, torch.Tensor)):
-                pass_back_gradients = [i.grad for i in toy_input]
-                for m_input in toy_input:
-                    m_input.requires_grad_(False)
-            else:
-                pass_back_gradients = toy_input.grad
-                toy_input.requires_grad_(False)
-            # the user will pass in what WAS the input for this stage!
+            pass_back_gradients = untrack_gradients(toy_input)
+
+                
         if scaler is not None:
             scaler.step(optimizer)
             scaler.update()
@@ -72,15 +60,8 @@ class Backward():
             optimizer.step()
             optimizer.zero_grad(set_to_none=True)
 
-
-        if not isinstance(toy_input, torch.Tensor):
-            while (len(toy_input) > 0):
-                del toy_input[0]
-            del toy_input
-        else:
-            del toy_input
+        delete_batch(toy_input)
 
         model.zero_grad(set_to_none=True)
-
-
+        
         return scaler, pass_back_gradients 
