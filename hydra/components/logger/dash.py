ec2ef6ac00b0e59779acad30c0e68f7991049752
knagrecha <32966638+knagrecha@users.noreply.github.com>
2022-09-29 11:29:55 +0530

    Update dash.py

--- hydra/components/logger/dash.py
+++ hydra/components/logger/dash.py
@@ -2,18 +2,19 @@ import curses
 import time
 
 
-def task_progress(name, epoch_count, minibatch, minibatch_count, m_time, t_time, y, x):
+def task_progress(name, epoch_count, minibatch, minibatch_count, m_time, t_time, loss, y, x):
     progress = int(minibatch / minibatch_count)
     stdscr.addstr(y, x, "{} | Epochs Remaining {}".format(name, epoch_count))
     stdscr.addstr(y+1, x, "Epoch Progress: [{1:10}] {0}%".format(progress * 10, "#" * progress))
     stdscr.addstr(y+2, x, "Minibatch Time: {:.2f}s | Remaining Time in Epoch: {:.2f}s".format(m_time, t_time))
+    stdscr.addstr(y+3, x, "Last Loss: {}".format(loss))
     stdscr.refresh()
 
-def report_progress(task_names, epoch_counts, minibatches, minibatch_counts, m_times, t_times):
+def report_progress(task_names, epoch_counts, minibatches, minibatch_counts, m_times, t_times, losses):
 
     for task_idx, task in enumerate(task_names):
         task_progress(task, epoch_counts[task_idx], minibatches[task_idx], minibatch_counts[task_idx], 
-                                                      m_times[task_idx], t_times[task_idx], 0 + 4 * task+task_idx, 0)
+                                                      m_times[task_idx], t_times[task_idx], losses[task_idx] 0 + 5 * task_idx, 0)
 
     
     
@@ -26,7 +27,7 @@ class Logger():
         curses.cbreak()
   
     def refresh(self):
-        names, epoch_counts, minibatches, minibatch_counts, m_times, t_times = [], [], [], [], [], []
+        names, epoch_counts, minibatches, minibatch_counts, m_times, t_times, losses = [], [], [], [], [], [], []
         for t in self.tasks:
             names.append(t.name)
             epoch_counts.append(t.epochs)
@@ -34,6 +35,7 @@ class Logger():
             minibatch_counts.append(t.total_length)
             m_times.append(t.total_time)
             t_times.append(t.total_time * t.batches_remaining)
+            losses.append(t.last_loss)
             
 
-    report_progress(names, epoch_counts, minibatches, minibatch_counts, m_times, t_times)
+    report_progress(names, epoch_counts, minibatches, minibatch_counts, m_times, t_times, losses)
