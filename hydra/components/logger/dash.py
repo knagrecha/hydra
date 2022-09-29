1600562e7dbbe2344d09600bebaf623f2c0743f9
knagrecha <32966638+knagrecha@users.noreply.github.com>
2022-09-29 11:36:16 +0530

    Update dash.py

--- hydra/components/logger/dash.py
+++ hydra/components/logger/dash.py
@@ -2,19 +2,7 @@ import curses
 import time
 
 
-def task_progress(name, epoch_count, minibatch, minibatch_count, m_time, t_time, loss, y, x):
-    progress = int(minibatch / minibatch_count)
-    stdscr.addstr(y, x, "{} | Epochs Remaining {}".format(name, epoch_count))
-    stdscr.addstr(y+1, x, "Epoch Progress: [{1:10}] {0}%".format(progress * 10, "#" * progress))
-    stdscr.addstr(y+2, x, "Minibatch Time: {:.2f}s | Remaining Time in Epoch: {:.2f}s".format(m_time, t_time))
-    stdscr.addstr(y+3, x, "Last Loss: {}".format(loss))
-    stdscr.refresh()
 
-def report_progress(task_names, epoch_counts, minibatches, minibatch_counts, m_times, t_times, losses):
-
-    for task_idx, task in enumerate(task_names):
-        task_progress(task, epoch_counts[task_idx], minibatches[task_idx], minibatch_counts[task_idx], 
-                                                      m_times[task_idx], t_times[task_idx], losses[task_idx], 0 + 5 * task_idx, 0)
 
     
     
@@ -22,9 +10,22 @@ def report_progress(task_names, epoch_counts, minibatches, minibatch_counts, m_t
 class Logger():
     def __init__(self, tasks):
         self.tasks = tasks
-        stdscr = curses.initscr()
+        self.stdscr = curses.initscr()
         curses.noecho()
         curses.cbreak()
+        
+    def task_progress(self, name, epoch_count, minibatch, minibatch_count, m_time, t_time, loss, y, x):
+        progress = int(minibatch / minibatch_count)
+        self.stdscr.addstr(y, x, "{} | Epochs Remaining {}".format(name, epoch_count))
+        self.stdscr.addstr(y+1, x, "Epoch Progress: [{1:10}] {0}%".format(progress * 10, "#" * progress))
+        self.stdscr.addstr(y+2, x, "Minibatch Time: {:.2f}s | Remaining Time in Epoch: {:.2f}s".format(m_time, t_time))
+        self.stdscr.addstr(y+3, x, "Last Loss: {}".format(loss))
+        self.stdscr.refresh()
+
+    def report_progress(self, task_names, epoch_counts, minibatches, minibatch_counts, m_times, t_times, losses):
+        for task_idx, task in enumerate(task_names):
+            self.task_progress(task, epoch_counts[task_idx], minibatches[task_idx], minibatch_counts[task_idx], 
+                                                          m_times[task_idx], t_times[task_idx], losses[task_idx], 0 + 5 * task_idx, 0)
   
     def refresh(self):
         names, epoch_counts, minibatches, minibatch_counts, m_times, t_times, losses = [], [], [], [], [], [], []
@@ -38,4 +39,4 @@ class Logger():
             losses.append(t.last_loss)
             
 
-        report_progress(names, epoch_counts, minibatches, minibatch_counts, m_times, t_times, losses)
+        self.report_progress(names, epoch_counts, minibatches, minibatch_counts, m_times, t_times, losses)
